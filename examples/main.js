5cbdbd627370c9d3955631cabf4c50ecab48d694
kuiq.W <569271010@qq.com>
2020-09-04 17:00:06 +0800

    注释

--- examples/main.js
+++ examples/main.js
@@ -4,106 +4,95 @@ import router from "./router/";
 
 import {
   KFormDesign,
-  KFormBuild,
-  setFormDesignConfig
+  KFormBuild
+  // setFormDesignConfig
 } from "../packages/index";
 // import Cmp from "./components/CustomComponent/index.vue";
 import selectSearch from "./components/CustomComponent/selectSearch.vue";
-setFormDesignConfig({
-  title: "测试自定义字段",
-  list: [
-    {
-      type: "select", // 表单类型
-      label: "自定义组件", // 标题文字
-      icon: "icon-gallery",
-      component: "",
-      options: {
-        originUrl: "origin",
-        width: "100%", // 宽度
-        defaultValue: undefined, // 下拉选框请使用undefined为默认值
-        multiple: false, // 是否允许多选
-        disabled: false, // 是否禁用
-        clearable: false, // 是否显示清除按钮
-        hidden: false, // 是否隐藏，false显示，true隐藏
-        placeholder: "请选择", // 默认提示文字
-        dynamicKey: "",
-        dynamicParam: "items",
-        dynamic: false,
-        dynamicUrl: "http://rap2.taobao.org:38080/app/mock/256811/checkbox", //获取外部数据接口url
-        dynamicType: {},
-        options: [
-          // 下拉选择项配置
-          {
-            value: "1",
-            label: "下拉框1"
-          },
-          {
-            value: "2",
-            label: "下拉框2"
-          }
-        ],
-        showSearch: false // 是否显示搜索框，搜索选择的项的值，而不是文字
-      },
-      model: "role",
-      key: "role",
-      rules: [
-        {
-          required: false,
-          message: "必填项"
-        }
-      ]
-    },
-    {
-      type: "selectSearch", // 表单类型
-      label: "指派人员", // 标题文字
-      icon: "icon-gallery",
-      component: selectSearch,
-      options: {
-        width: "100%", // 宽度
-        defaultValue: undefined, // 下拉选框请使用undefined为默认值
-        multiple: false, // 是否允许多选
-        disabled: false, // 是否禁用
-        clearable: false, // 是否显示清除按钮
-        hidden: false, // 是否隐藏，false显示，true隐藏
-        placeholder: "请选择", // 默认提示文字
-        dynamicKey: "__NEXT_USER__",
-        dynamicParam: "data",
-        dynamic: true,
-        dynamicUrl:
-          "http://10.199.134.132/itsm/api/v1/itsm/dynamic_data/user_list/?search=", //获取外部数据接口url
-        dynamicType: {},
-        options: [
-          // 下拉选择项配置
-          {
-            value: "1",
-            label: "下拉框1"
-          },
-          {
-            value: "2",
-            label: "下拉框33"
-          }
-        ],
-        showSearch: true // 是否显示搜索框，搜索选择的项的值，而不是文字
-      },
-      model: "__NEXT_USER__",
-      key: "__NEXT_USER__",
-      rules: [
-        {
-          required: false,
-          message: "必填项"
-        }
-      ]
-    }
-  ],
-  uploadFile: "",
-  uploadImage: "",
-  uploadFileName: "",
-  uploadImageName: "",
-  uploadFileData: { data: 1545 },
-  uploadImageData: { data: 1545 },
-  uploadFileHeaders: { data: 1545 },
-  uploadImageHeaders: { data: 1545 }
-});
+// setFormDesignConfig({
+//   title: "测试自定义字段",
+//   list: [
+//     {
+//       type: "select", // 表单类型
+//       label: "自定义组件", // 标题文字
+//       icon: "icon-gallery",
+//       component: "",
+//       options: {
+//         originUrl: "http://rap2.taobao.org:38080/app/mock/256811/originPage",
+//         width: "100%", // 宽度
+//         defaultValue: undefined, // 下拉选框请使用undefined为默认值
+//         multiple: false, // 是否允许多选
+//         disabled: false, // 是否禁用
+//         clearable: false, // 是否显示清除按钮
+//         hidden: false, // 是否隐藏，false显示，true隐藏
+//         placeholder: "请选择", // 默认提示文字
+//         dynamicKey: "",
+//         dynamicParam: "items",
+//         dynamic: false,
+//         dynamicUrl: "http://rap2.taobao.org:38080/app/mock/256811/checkbox", //获取外部数据接口url
+//         dynamicType: {},
+//         showSearch: false // 是否显示搜索框，搜索选择的项的值，而不是文字
+//       },
+//       model: "role",
+//       key: "role",
+//       rules: [
+//         {
+//           required: false,
+//           message: "必填项"
+//         }
+//       ]
+//     },
+//     {
+//       type: "selectSearch", // 表单类型
+//       label: "指派人员", // 标题文字
+//       icon: "icon-gallery",
+//       component: selectSearch,
+//       options: {
+//         width: "100%", // 宽度
+//         defaultValue: undefined, // 下拉选框请使用undefined为默认值
+//         multiple: false, // 是否允许多选
+//         disabled: false, // 是否禁用
+//         clearable: false, // 是否显示清除按钮
+//         hidden: false, // 是否隐藏，false显示，true隐藏
+//         placeholder: "请选择", // 默认提示文字
+//         dynamicKey: "__NEXT_USER__",
+//         dynamicParam: "data",
+//         dynamic: true,
+//         dynamicUrl:
+//           "http://10.199.134.132/itsm/api/v1/itsm/dynamic_data/user_list/?search=", //获取外部数据接口url
+//         dynamicType: {},
+//         options: [
+//           // 下拉选择项配置
+//           {
+//             value: "1",
+//             label: "下拉框1"
+//           },
+//           {
+//             value: "2",
+//             label: "下拉框33"
+//           }
+//         ],
+//         showSearch: true // 是否显示搜索框，搜索选择的项的值，而不是文字
+//       },
+//       model: "__NEXT_USER__",
+//       key: "__NEXT_USER__",
+//       rules: [
+//         {
+//           required: false,
+//           message: "必填项"
+//         }
+//       ]
+//     }
+//   ],
+//   uploadFile: "",
+//   uploadImage: "",
+//   uploadFileName: "",
+//   uploadImageName: "",
+//   uploadFileData: { data: 1545 },
+//   uploadImageData: { data: 1545 },
+//   uploadFileHeaders: { data: 1545 },
+//   uploadImageHeaders: { data: 1545 }
+// });
 Vue.use(KFormDesign);
 Vue.use(KFormBuild);
 Vue.config.productionTip = false;
