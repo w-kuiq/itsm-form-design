34cffc315e3360558752cf3c4f9d49975373a19e
kuiq.W <569271010@qq.com>
2020-09-11 15:26:50 +0800

    version 19

--- packages/KEditor/mceEditor.vue
+++ packages/KEditor/mceEditor.vue
@@ -69,10 +69,10 @@ export default {
   data() {
     return {
       init: {
-        language_url: `${this.baseUrl}/tinymce/langs/zh_CN.js`,
+        language_url: `${this.baseUrl}/static/tinymce/langs/zh_CN.js`,
         language: "zh_CN",
-        skin_url: `${this.baseUrl}/tinymce/skins/ui/oxide`,
-        content_css: `${this.baseUrl}/tinymce/skins/content/default/content.css`,
+        skin_url: `${this.baseUrl}/static/tinymce/skins/ui/oxide`,
+        content_css: `${this.baseUrl}/static/tinymce/skins/content/default/content.css`,
         // skin_url: `${this.baseUrl}/tinymce/skins/ui/oxide-dark`, // 暗色系
         // content_css: `${this.baseUrl}/tinymce/skins/content/dark/content.css`, // 暗色系
         height: 300,
