8f0f59d13adfbf39f6462d14ddd066169918a704
kuiq.W <569271010@qq.com>
2020-10-09 16:15:49 +0800

    富文本测试

--- packages/KEditor/index.js
+++ packages/KEditor/index.js
@@ -3,7 +3,7 @@
  * @Author: kcz
  * @Date: 2020-03-30 12:45:04
  * @LastEditors: wangkq
- * @LastEditTime: 2020-09-21 14:18:05
+ * @LastEditTime: 2020-10-09 16:03:12
  */
-import KEditor from "./kEditor.vue";
+import KEditor from "./mceEditor.vue";
 export default KEditor;
