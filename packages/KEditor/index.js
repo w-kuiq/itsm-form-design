ed90cb9620160b43784cd48880a3e7a3e0b748d2
kuiq.W <569271010@qq.com>
2020-10-12 20:37:23 +0800

    1.4.29

--- packages/KEditor/index.js
+++ packages/KEditor/index.js
@@ -3,7 +3,7 @@
  * @Author: kcz
  * @Date: 2020-03-30 12:45:04
  * @LastEditors: wangkq
- * @LastEditTime: 2020-10-09 16:03:12
+ * @LastEditTime: 2020-10-12 20:32:48
  */
-import KEditor from "./mceEditor.vue";
+import KEditor from "./kEditor.vue";
 export default KEditor;
