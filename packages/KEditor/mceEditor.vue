77f988023634d75ede909a79fc0dcc3795226a1b
kuiq.W <569271010@qq.com>
2020-11-04 15:42:05 +0800

    33

--- packages/KEditor/mceEditor.vue
+++ packages/KEditor/mceEditor.vue
@@ -17,13 +17,13 @@ import Editor from "@tinymce/tinymce-vue";
 import "tinymce/themes/silver";
 // 编辑器插件plugins
 // 更多插件参考：https://www.tiny.cloud/docs/plugins/
-import "tinymce/plugins/image"; // 插入上传图片插件
-import "tinymce/plugins/media"; // 插入视频插件
-import "tinymce/plugins/table"; // 插入表格插件
-import "tinymce/plugins/lists"; // 列表插件
-import "tinymce/plugins/wordcount"; // 字数统计插件
-import "tinymce/plugins/paste";
-import "tinymce/icons/default/icons";
+// import "tinymce/plugins/image"; // 插入上传图片插件
+// import "tinymce/plugins/media"; // 插入视频插件
+// import "tinymce/plugins/table"; // 插入表格插件
+// import "tinymce/plugins/lists"; // 列表插件
+// import "tinymce/plugins/wordcount"; // 字数统计插件
+// import "tinymce/plugins/paste";
+// import "tinymce/icons/default/icons";
 import toolbar from "./toolbar.js";
 import plugins from "./plugins.js";
 
