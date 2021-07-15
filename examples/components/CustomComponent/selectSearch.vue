fce71a512daa40ad32911dcea2391ed8b80603a1
kuiq.W <569271010@qq.com>
2021-07-15 18:55:25 +0800

    1.5.13

--- examples/components/CustomComponent/selectSearch.vue
+++ examples/components/CustomComponent/selectSearch.vue
@@ -4,14 +4,14 @@
  * @LastEditTime: 2020-07-22 18:19:49
  * @LastEditors: wangkq
  * @Description: 
---> 
+-->
 <!--
  * @Author: wangkq
  * @Date: 2020-06-19 11:04:43
  * @LastEditTime: 2020-07-16 15:45:07
  * @LastEditors: wangkq
  * @Description: 
---> 
+-->
 <template>
   <div>
     <a-select
@@ -22,7 +22,7 @@
       :show-arrow="false"
       :filter-option="false"
       :not-found-content="null"
-      :disabled='disabled'
+      :disabled="disabled"
       @search="handleSearch"
       @change="handleChange"
     >
@@ -33,15 +33,15 @@
   </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  name: 'selectSearch',
-  data(){
-    return{
-      data:[]
-    }
+  name: "selectSearch",
+  data() {
+    return {
+      data: []
+    };
   },
-  props: ['record', 'value','disabled','dynamicData'],
+  props: ["record", "value", "disabled", "dynamicData"],
   // props: {
   //   record: {
   //     type: Object,
@@ -53,30 +53,43 @@ export default {
   //   }
   // },
   methods: {
-    handleChange (e) {
+    handleChange(e) {
       // 使用 onChange 事件修改值
-      this.$emit('change', e)
+      this.$emit("change", e);
     },
     handleSearch(value) {
       // this.request(this.record.options.dynamicUrl+value).then(res=>{
       //   this.data = res
       // })
+      let url = this.record.options.dynamicUrl + value;
+
+      if (window.sessionStorage.getItem("process_instance_id")) {
+        let process_instance_id = window.sessionStorage.getItem(
+          "process_instance_id"
+        );
+        url =
+          this.record.options.dynamicUrl.split("?")[0] +
+          "?process_instance_id=" +
+          process_instance_id +
+          "&" +
+          this.record.options.dynamicUrl.split("?")[1] +
+          value;
+      }
       axios
-        .get(this.record.options.dynamicUrl+value)
+        .get(url)
         .then(res => {
           if (res.status == 200) {
-           this.data = res.data[this.record.options.dynamicParam];
+            this.data = res.data[this.record.options.dynamicParam];
           }
         })
         .catch(err => {
           this.$message.error(err);
         });
-    },
-  
+    }
   },
-  mounted () {
+  mounted() {
     // 打印接收的options
-    window.$$ = this
+    window.$$ = this;
   }
-}
+};
 </script>
